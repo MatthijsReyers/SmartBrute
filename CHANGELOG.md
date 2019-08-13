3b5267cc4cf4f777570cc815c18f9644c46567c2
MatthijsReyers <matthijscrafter@gmail.com>
2019-08-13 12:53:12 +0200

    I fixed so many bugs its not even funny

--- CHANGELOG.md
+++ CHANGELOG.md
@@ -1,6 +1,10 @@
 # SmartBrute changelog
 
-### 12/08/2019 - Dutch password list examples
+### 12/08/2019 - GetAllComb() optimalization and new features
+The GetAllComb() function now no longer generates a list with all possible combinations but instead returns an object that will generate the required string when asked for or itterated over (similarly to how python's build-in range function works). This means that python no longer needs to store giant amounts of data in RAM when working with multiple nested GetAllComb() functions or when the GetAllComb() function is given a large list.
+I have also added the ```minKeys``` & ```maxKeys``` arguments which allow the user to choose how many keys to combine, which was previously not possible.
+
+### 12/06/2019 - Dutch password list examples
 I wanted to 
 
 ### 25/07/2019 - Adding the RemoveDoubles() function
@@ -11,3 +15,8 @@ I have also deprecated the GetAllCombForEach() function after realizing that it 
 It is now possible to filter the generated passwords for certain characters, and you can also disable the previewing of generated passwords in the console, because it significatnly slows down the program.
 
 
+
+# Upcomming/requested/suggested features
+
+- Filtering generated passwords with regex/regular expressions.
+- The ability to load in a password list from a file for manipulation in with SmartBrute.
--- beta_features/GetAllCombNew.py
+++ beta_features/GetAllCombNew.py
@@ -7,7 +7,7 @@ class GetAllCombNew:
         self.current = 0
         self.len = 0
         for i in range(minKeys, maxKeys+1):
-            self.len = self.len + (i**self.keysLen)
+            self.len = self.len + (self.keysLen**i)
 
     def convertToBase(self, number:int, bits:int, base:int):
         out = [0 for i in range(bits)]
@@ -21,21 +21,39 @@ class GetAllCombNew:
         out.reverse()
         return out
 
-    def __getitem__(self, s):
-        return 2
+    def __getitem__(self, itemIndex):
+        # Figure out where the hell in the indexes we are.
+        keyCount = 0
+        stuffcounter = 0
+        index = itemIndex
+
+        for i in range(self.min, self.max+1):
+            if itemIndex < (self.keysLen**i + stuffcounter):
+                keyCount = i
+                break
+            else:
+                index = index - (self.keysLen**i)
+                stuffcounter = stuffcounter + (self.keysLen**i)
+
+        # Do stuff
+        keyIndexes = self.convertToBase(index, keyCount, self.keysLen)
+        outKey = ''
+
+        # Create string
+        for ki in keyIndexes: outKey = outKey + self.keys[ki]
+        return outKey
 
     def __iter__(self):
         return self
 
     def __next__(self):
-        if self.current > self.len:
+        if self.current >= self.len:
             raise StopIteration
         else:
             self.current += 1
-            return self.__getitem__(self.current)
+            return self.__getitem__(self.current - 1)
 
 if __name__ == '__main__':
 
-    for test in GetAllCombNew(['test','wow'],4,4):
-
-        print(test)
+    for i in GetAllCombNew(['AA','BB','CC'],1,4):
+        print(i)
